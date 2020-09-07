08bdb884edd4e381b5d60e88b20e7ba537065634
charles.chang <charles.chang@royaltek.com>
2020-09-07 11:00:03 +0800

    get effect g and print

--- process.py
+++ process.py
@@ -1,3 +1,5 @@
+import math
+
 fp = open('./gvalue0.txt','r')
 line = fp.readline(); # skip one line
 line = fp.readline().strip();
@@ -6,6 +8,7 @@ while line:
 	x = float(values[0]);
 	y = float(values[1]);
 	z = float(values[2]);
-	print(x,y,z)
+	g = math.sqrt(x**2 + y**2 + z**2)
+	print(x,y,z,g)
 	line = fp.readline().strip();
 
