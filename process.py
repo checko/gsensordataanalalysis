6a067bf9bb5f8039d4439aa8f2df4532109bdcef
charles.chang <charles.chang@royaltek.com>
2020-09-07 11:10:42 +0800

    g array

--- process.py
+++ process.py
@@ -2,6 +2,7 @@ import math
 
 fp = open('./gvalue0.txt','r')
 line = fp.readline(); # skip one line
+gdata = []
 line = fp.readline().strip();
 while line:
 	values = line.split(',')
@@ -9,6 +10,7 @@ while line:
 	y = float(values[1]);
 	z = float(values[2]);
 	g = math.sqrt(x**2 + y**2 + z**2)
+	gdata.append(g)
 	print(x,y,z,round(g,2))
 	line = fp.readline().strip();
-
+print(gdata)
