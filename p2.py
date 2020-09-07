2cc927931d988f15bb20d507bec712b8ba53662a
charles.chang <charles.chang@royaltek.com>
2020-09-07 15:04:47 +0800

    p2.py: x-y-z proportion

---
+++ p2.py
@@ -0,0 +1,30 @@
+import math
+import matplotlib.pyplot as pp
+import sys
+
+if (len(sys.argv) < 2):
+	print('no data file\n')
+	exit()
+fp = open(sys.argv[1],'r')
+line = fp.readline(); # skip one line
+gdata = []
+line = fp.readline().strip();
+while line:
+	values = line.split(',')
+	x = float(values[0]);
+	y = float(values[1]);
+	z = float(values[2]);
+	g = math.sqrt(x**2 + y**2 + z**2)
+	gdata.append(g)
+	print(round(x/y,3),round(y/y,2),round(z/y,2),round(g,2))
+	line = fp.readline().strip();
+#print(gdata)
+if(len(sys.argv)==4):
+	start = int(sys.argv[2])
+	end = int(sys.argv[3])
+	pp.plot(gdata[start:end])
+else:
+	start = 0
+	end = len(gdata)
+pp.plot(gdata[start:end])
+pp.show()
