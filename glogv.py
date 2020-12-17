2213d88f3f9993a5a2612f847ef862a49f76aa18
charles.chang <charles.chang@royaltek.com>
2020-12-17 11:42:48 +0800

    plot y,z

--- glogv.py
+++ glogv.py
@@ -10,6 +10,7 @@ fp = open(sys.argv[1],'r')
 vdata = []
 xdata = []
 ydata = []
+zdata = []
 line = fp.readline()
 print(line)
 while line:
@@ -20,11 +21,15 @@ while line:
     v = float(value[4])
     vdata.append(v)
     xdata.append(x)
+    ydata.append(y)
+    zdata.append(z)
     line = fp.readline()
 fig, ax1 = plt.subplots(1,1)
 ax2 = ax1.twinx()
 
 ax1.plot(vdata,color='r',alpha=0.5)
 ax2.plot(xdata,alpha=0.5)
+ax2.plot(ydata,color='b',alpha=0.5)
+ax2.plot(zdata,color='g',alpha=0.5)
 
 plt.show()
